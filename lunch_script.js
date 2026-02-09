decf939fb9c6c642ec5e2cb2cc64c8b5ec8f0303
jiavaani1229 <jiavaani1229@gmail.com>
2026-02-08 19:05:01 -0500

    Update lunch menu for the week

--- lunch_script.js
+++ lunch_script.js
@@ -2,11 +2,11 @@
 // Update this object with the menu for each day of the week.
 const lunchMenu = {
     // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
-    1: "Orange Chicken and Potatoes",
-    2: "Chicken Tenders",
-    3: "Beef Lasagna and Garlic Bread",
-    4: "Lemon Pepper Chicken",
-    5: "Pizza",
+    1: "Balsamic Chicken and Mashed Potatoes",
+    2: "Tacos",
+    3: "Chicken Pot Pie",
+    4: "French Toast",
+    5: "Burgurs and Fries",
     6: "No School Lunch Today",
     0: "No School Lunch Today"
 };
